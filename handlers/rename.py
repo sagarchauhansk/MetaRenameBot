5c71fc021166d28f52677ef972bc877d8905abe0
Sagar Chauhan <119773206+sagarchauhansk@users.noreply.github.com>
2025-03-03 20:22:13 +0530

    Update rename.py

--- handlers/rename.py
+++ handlers/rename.py
@@ -1,15 +1,17 @@
 from pyrogram import Client, filters
+import os
 
 @Client.on_message(filters.command("rename") & filters.reply)
-def rename_file(client, message):
+async def rename_file(client, message):
     if not message.reply_to_message.document:
-        message.reply_text("Reply to a file with /rename [new_name.extension]")
+        await message.reply_text("⚠️ Reply to a file with `/rename new_name.extension`")
         return
 
     try:
         new_name = message.text.split(" ", 1)[1]
-        file_id = message.reply_to_message.document.file_id
-        message.reply_to_message.download(file_name=new_name)
-        message.reply_text(f"✅ File renamed to {new_name}!")
+        file = await message.reply_to_message.download()
+        os.rename(file, new_name)
+        await message.reply_document(new_name, caption=f"✅ Renamed to `{new_name}`")
+        os.remove(new_name)
     except IndexError:
-        message.reply_text("⚠️ Please provide a new file name. Example: /rename newfile.pdf")
+        await message.reply_text("⚠️ Please provide a new file name. Example: `/rename newfile.pdf`")
